79e2644f49b2d2028a20768b8b3970aa55652738
Nehardh Madapuri <136841922+nehardh@users.noreply.github.com>
2025-04-20 17:45:05 +0530

    Update page.jsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+"use client"
 import { navItems } from "@/data";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatinNav";
 import RecentProjects from "@/components/RecentProjects";
-import Activity from "@/components/Activity";
+// import Activity from "@/components/Activity";
 import Experience from "@/components/Experience";
 //import Approach from "@/components/Approach";
 import Footer from "@/components/Footer";
